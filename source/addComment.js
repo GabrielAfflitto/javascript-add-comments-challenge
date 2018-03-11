b83e34f14e5262837ed8013935229caef1f1f703
GabrielAfflitto <gafflitto92@gmail.com>
2018-03-11 16:58:29 -0600

    adding comments enabled

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-  // add your code here
+  $("#new_comment_button").click(function(){
+    $('#new_comment_button').hide();
+    $('.comments').append("<form id='new_comment'><textarea class='comment' name='comment' required></textarea><input id='authorName' name='author'><input type='submit' class='add-comment'></form>");
+    $('#new_comment').show();
+    $('.add-comment').submit(function(){
+      const comment = $('textarea[name="comment"]').val();
+      const author = $('input[name="author"]').val();
+      $('#comment_list').append("<li>" + comment + "<span class='author'>" + author + "</span></li>");
+      $('#new_comment').hide();
+      $('#new_comment_button').show();
+      event.preventDefault();
+    })
+  })
 })
